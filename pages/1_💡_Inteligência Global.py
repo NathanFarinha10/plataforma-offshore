f031fa01f4e8ee1ee5ab2322041e4aba6d104a4c
NathanFarinha10 <nathan.farinha10@gmail.com>
2025-08-10 20:29:34 -0300

    Create 1_💡_Inteligência Global.py

---
+++ pages/1_💡_Inteligência Global.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Inteligência Global",
+    page_icon="💡",
+    layout="wide"
+)
+
+st.title("💡 Inteligência Global")
+st.markdown("---")
+
+st.write("Em breve: Um consolidador de estratégias e visões macro das principais gestoras globais.")
+
+# Futuramente, aqui entrará a lógica para buscar e exibir os dados das gestoras.
